1a667b8472cb8863c71c48c8cea8450b44191d1d
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-14 21:45:56 +0500

    add main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/main.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster
+          position={"top-right"}
+          toastOptions={{
+            style: {
+              margin: "15px",
+              background: "#828282",
+              color: "#fff",
+              width: "340px",
+            },
+            className: "text-base",
+            duration: 3000,
+          }}
+        />
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
