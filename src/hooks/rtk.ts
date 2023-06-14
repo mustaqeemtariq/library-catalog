b8702c48c1f5f6b3ac42e6b5505e377a7513e7ce
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-14 21:45:11 +0500

    add selector and dispatch

---
+++ src/hooks/rtk.ts
@@ -0,0 +1,6 @@
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import type { AppDispatch, RootState } from '../store'
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
